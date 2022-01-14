4eb13ef8e766a4fd2e70c6168d0ef585cc3f53ca
williamcool7890 <62357797+williamcool7890@users.noreply.github.com>
2022-01-14 18:04:44 +0000

    Icon Reheight

--- components/Meta.js
+++ components/Meta.js
@@ -3,7 +3,7 @@ import Head from "next/head";
 const Meta = ({ title, keywords, description, favicon }) => {
 	return (
 		<Head>
-			<link rel="icon" href={favicon} />
+			<link rel="icon" href={favicon} className="h-100" />
 			<meta name="viewport" content="width=device-width initial-scale=1" />
 			<meta name="keywords" content={keywords} />
 			<meta name="description" content={description} />
