967d134278ff75de6e84742c15e6270e25e1823b
williamcool7890 <62357797+williamcool7890@users.noreply.github.com>
2022-01-30 12:26:39 +0000

    Contact Us

--- pages/contact-us.js
+++ pages/contact-us.js
@@ -1,9 +1,81 @@
 import React from "react";
+import styles from "../styles/Contact.module.css";
+import {
+	FaEnvelope,
+	FaPhoneAlt,
+	FaFacebookSquare,
+	FaInstagram,
+	FaTwitter,
+} from "react-icons/fa";
+import Map from "../components/Map";
 
 const contact = () => {
 	return (
 		<div>
-			<h1>Contact Us</h1>
+			<section>
+				<div className="map">
+					<Map />
+				</div>
+				<div className="text">
+					<div className="">
+						<h1>Located At</h1>
+						<p>
+							Territorial Army Center,
+							<br />
+							Guildford RD, Farnham
+							<br />
+							GU9 9QB
+						</p>
+					</div>
+					<br />
+					<div className="">
+						<h1>Parade Evenings</h1>
+						<p>Tuesday 19:00 - 21:30</p>
+						<p>Friday 19:00 - 21:30</p>
+					</div>
+				</div>
+			</section>
+			<section className={styles.slantedDiv}>
+				<hr className="border-[2px] h-0" />
+				<div className="flex justify-evenly p-6">
+					<section>
+						<h1>Contact Us</h1>
+						<div className="flex">
+							<a className="self-center">
+								<FaEnvelope />
+							</a>
+							<span>229@aircadets.ord</span>
+						</div>
+						<div className="flex">
+							<a className="self-center">
+								<FaPhoneAlt />
+							</a>
+							<span>01252721810</span>
+						</div>
+					</section>
+					<section>
+						<h1>Social Media</h1>
+						<div className="flex">
+							<a className="self-center">
+								<FaFacebookSquare />
+							</a>
+							<span>229 (Farnham) sqn atc</span>
+						</div>
+						<div className="flex">
+							<a className="self-center">
+								<FaInstagram />
+							</a>
+							<span>@229sqn</span>
+						</div>
+						<div className="flex">
+							<a className="self-center">
+								<FaTwitter />
+							</a>
+							<span>@229sqn</span>
+						</div>
+					</section>
+				</div>
+			</section>
 		</div>
 	);
 };
